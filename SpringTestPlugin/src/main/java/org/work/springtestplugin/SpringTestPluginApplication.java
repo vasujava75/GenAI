0d58f268173632480085c0d1a5bc4d92f5610ec6
HE06BW <vasudev.kondapeti@ing.com>
2025-08-28 12:20:05 +0200

    added testing project

---
+++ SpringTestPlugin/src/main/java/org/work/springtestplugin/SpringTestPluginApplication.java
@@ -0,0 +1,13 @@
+package org.work.springtestplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestPluginApplication.class, args);
+    }
+
+}
---
+++ SpringTestPlugin/src/test/java/org/work/springtestplugin/SpringTestPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.work.springtestplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestPluginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
